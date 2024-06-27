d9e73f7798912e026e81036e421c57a11d2218e8
kknaks <benesia93@naver.com>
2024-06-27 16:04:31 +0900

    Test01_m1()

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -4,7 +4,9 @@ public class Test1 {
     void test1(){
         System.out.println("git Test 입니다.");
     }
-
+    static void m1() {
+        System.out.println("오늘 저녁 머 먹지?");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
