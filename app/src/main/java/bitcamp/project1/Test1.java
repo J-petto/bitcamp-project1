1d8808a390012699527af3202e90712fe57e812d
황민지 <hminji4565@gmail.com>
2024-06-27 16:50:49 +0900

    main 변경 테스트

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -1,7 +1,7 @@
 package bitcamp.project1;
 
 public class Test1 {
-    void test1(){
+    static void test1(){
         System.out.println("git Test 입니다.");
     }
 
@@ -14,10 +14,14 @@ public class Test1 {
         System.out.println("오늘 저녁 머 먹지?");
     }
     public static void main(String[] args) {
+        Test1 test1 = new Test1();
         System.out.println("Hello World");
+        test1();
+        test1.m2();
     }
 
     static void m3() {
         System.out.println("오호라!!!");
     }
+
 }
