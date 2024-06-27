5387a780de3e93476bc8258425b237b5d21c9948
eomjinyoung <jinyoung.eom@gmail.com>
2024-06-27 16:05:43 +0900

    강사가 변경함

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -8,4 +8,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    static void m3() {
+        System.out.println("오호라!!!");
+    }
 }
