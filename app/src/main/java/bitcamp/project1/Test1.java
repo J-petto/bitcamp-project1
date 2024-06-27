3c3c4389dbcc8245e649de8820c59430ee37d137
eomjinyoung <jinyoung.eom@gmail.com>
2024-06-27 16:51:48 +0900

    .

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -7,10 +7,17 @@ public class Test1 {
     static void m1() {
         System.out.println("오늘 저녁 머 먹지?");
     }
+    static void m4() {
+        System.out.println("okoknonon");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2222");
+        System.out.println("OkOk")
     }
 
+    static void m2() {
+        System.out.println("강사..");
+    }
     static void m3() {
         System.out.println("오호라!!!");
     }
