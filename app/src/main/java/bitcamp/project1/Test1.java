53bc9283650c77d0af1d25b83608394e377ad870
황민지 <hminji4565@gmail.com>
2024-06-27 16:04:31 +0900

    feat: m2 method

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
         System.out.println("git Test 입니다.");
     }
 
+    void m2(){
+        int m2 = 999999999;
+        System.out.println(m2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
