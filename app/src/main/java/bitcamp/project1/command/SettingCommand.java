12e795fd5d688039d7317dae9ea156db14d95105
황민지 <hminji4565@gmail.com>
2024-06-28 19:59:38 +0900

    Setting 데이터 타입 변경

--- app/src/main/java/bitcamp/project1/command/SettingCommand.java
+++ app/src/main/java/bitcamp/project1/command/SettingCommand.java
@@ -7,9 +7,11 @@ import bitcamp.project1.vo.Cash;
 import bitcamp.project1.vo.CreditCard;
 
 public class SettingCommand {
-  ArrayList totalCash = new ArrayList();
-  ArrayList totalBank = new ArrayList();
-  ArrayList totalCredit = new ArrayList();
+  final int CASH = 0;
+  final int BANK = 1;
+  final int CREDIT = 2;
+
+  private Object[] userSettingList = new Object[3];
 
   private boolean settingDone = true;
 
@@ -40,18 +42,18 @@ public class SettingCommand {
       Cash cash = new Cash();
       System.out.println("현금");
       cash.setCuurentAmount(Prompt.inputInt("잔액?"));
-      totalCash.add(cash);
+      userSettingList[CASH] = cash;
 
       BankAccount bankAccount = new BankAccount();
       System.out.println("통장");
       bankAccount.setBankName(Prompt.input("은행명?"));
       bankAccount.setDepositAmount(Prompt.inputInt("잔액?"));
-      totalBank.add(bankAccount);
+      userSettingList[BANK] = bankAccount;
 
       CreditCard creditCard = new CreditCard();
       System.out.println("신용카드");
       creditCard.setCardName(Prompt.input("카드사명?"));
-      totalCredit.add(creditCard);
+      userSettingList[CREDIT] = creditCard;
       settingDone = false;
     } else {
       System.out.println("이미 계정이 생성되어 있습니다.");
@@ -63,7 +65,19 @@ public class SettingCommand {
 
   public void searchSetting() {}
 
-  public void updateSetting() {}
+  public void updateSetting() {
+    String command = Prompt.input("보유 현금을 수정하시겠습니까?(Y/N)");
+    if(command.equalsIgnoreCase("Y")){
 
+    }
+    command = Prompt.input("보유 통장을 수정하시겠습니까?(Y/N)");
+    if(command.equalsIgnoreCase("Y")){
+      int cash = Prompt.inputInt("수정 현금?");
+    }
+  }
   public void deleteSetting() {}
+
+  public Object[] getUserSettingList() {
+    return userSettingList;
+  }
 }
