9cd0b13291a5e39981e23979717881190bc5f0e2
kknaks <benesia93@naver.com>
2024-06-28 16:38:26 +0900

    .

--- app/src/main/java/bitcamp/project1/command/SettingCommand.java
+++ app/src/main/java/bitcamp/project1/command/SettingCommand.java
@@ -1,4 +1,24 @@
 package bitcamp.project1.command;
 
 public class SettingCommand {
+  public void executeSettingCommand(String subTitle) {
+    switch (subTitle) {
+      case "등록":
+        createSetting();
+        break;
+      case "목록":
+        listISetting();
+        break;
+      case "조회":
+        searchSetting();
+        break;
+      case "변경":
+        updateSetting();
+        break;
+      case "삭제":
+        deleteSetting();
+        break;
+      default:
+    }
+  }
 }
