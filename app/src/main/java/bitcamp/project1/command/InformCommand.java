027b24884bfffdf5bbbe537cf3024ca1a35d5293
kknaks <benesia93@naver.com>
2024-06-28 10:12:21 +0900

    .

--- app/src/main/java/bitcamp/project1/command/InformCommand.java
+++ app/src/main/java/bitcamp/project1/command/InformCommand.java
@@ -10,26 +10,28 @@ import java.util.HashSet;
 public class InformCommand {
   ArrayList incomeList = new ArrayList();
   ArrayList outcomeList = new ArrayList();
+  ArrayList totalList = new ArrayList();
 
-  public InformCommand(ArrayList incomeList,ArrayList outcomeList) {
+  public InformCommand(ArrayList incomeList, ArrayList outcomeList) {
     this.incomeList = incomeList;
     this.outcomeList = outcomeList;
   }
 
-  public void executeInformCommand(String subTitle){
-    switch (subTitle){
-      case "총 지출 수입" :
+  public void executeInformCommand(String subTitle) {
+    switch (subTitle) {
+      case "총 지출 수입":
         viewTotal();
         break;
       case "일자별 수입 지출":
         viewDate();
         break;
-      case  "항목별 수입 지출":
+      case "항목별 수입 지출":
         viewCategory();
         break;
     }
   }
-  public Object[] concat(){
+
+  public Object[] concat() {
     int incomeSize = incomeList.size();
     int outcomeSize = outcomeList.size();
     Object[] result = new Object[incomeSize + outcomeSize];
@@ -42,9 +44,8 @@ public class InformCommand {
     return result;
   }
 
-
   // 1.총 지출 수입 목록
-  private void viewTotal(){
+  private void viewTotal() {
     System.out.println("총수입 총지출 총합");
 
     int incomeTotal = allSum(true);
@@ -54,16 +55,16 @@ public class InformCommand {
     System.out.printf("%d %d %d\n", incomeTotal, outcomeTotal, total);
   }
 
-  private int allSum(boolean inOut){
+  private int allSum(boolean inOut) {
     int sum = 0;
 
-    if(inOut){
-      for(Object obj : incomeList.toArray()){
+    if (inOut) {
+      for (Object obj : incomeList.toArray()) {
         Income plusIncome = (Income) obj;
         sum += plusIncome.getAmount();
       }
-    }else {
-      for(Object obj : outcomeList.toArray()){
+    } else {
+      for (Object obj : outcomeList.toArray()) {
         Income plusOutcome = (Income) obj;
         sum += plusOutcome.getAmount();
       }
@@ -72,42 +73,44 @@ public class InformCommand {
   }
 
   // 2. 일자별 수입 지출 목록
-  private void viewDate(){
-    Object[] total = union();;
+  private void viewDate() {
+    Object[] total = union();
+    ;
     Object[] uniqueDate = uniqueList();
     System.out.println("날짜 수입 지출 총계");
-    for (Object obj : uniqueDate){
+    for (Object obj : uniqueDate) {
       LocalDate date = (LocalDate) obj;
       int totalIncome = 0, totalOutcome = 0;
-      for (int i = 0; i < incomeList.size(); i++){
+      for (int i = 0; i < incomeList.size(); i++) {
         Income income = (Income) incomeList.get(i);
         totalIncome += income.getAmount();
       }
-      for (int i = 0; i < outcomeList.size(); i++){
+      for (int i = 0; i < outcomeList.size(); i++) {
         Income outcome = (Income) outcomeList.get(i);
         totalOutcome += outcome.getAmount();
       }
-      System.out.printf("%s, %d, %d", date.toString(), totalIncome, totalOutcome);
-    };
+      System.out.printf("%s, %d, %d\n", date.toString(), totalIncome, totalOutcome);
+    }
+    ;
 
   }
-  ArrayList totalList = new ArrayList();
-  private void totalArray(){
-    for(Object obj : incomeList.toArray()){
+
+  private void totalArray() {
+    for (Object obj : incomeList.toArray()) {
       Income addIncome = (Income) obj;
       totalList.add(addIncome);
     }
-    for(Object obj : outcomeList.toArray()){
+    for (Object obj : outcomeList.toArray()) {
       Income addOutcome = (Income) obj;
       totalList.add(addOutcome);
     }
   }
 
   //3. 품목별 수입 지출 목록
-  private void viewCategory(){
+  private void viewCategory() {
   }
 
-  private Object[] union(){
+  private Object[] union() {
     int incomeSize = incomeList.size();
     int outcomeSize = outcomeList.size();
     Object[] result = new Object[incomeSize + outcomeSize];
@@ -120,9 +123,9 @@ public class InformCommand {
     return result;
   }
 
-  private Object[] uniqueList(){
-    HashSet<LocalDate> set = new HashSet();
-    for (int i = 0; i < outcomeList.size(); i++){
+  private Object[] uniqueList() {
+    HashSet<LocalDate> set = new HashSet<>();
+    for (int i = 0; i < outcomeList.size(); i++) {
       Income outcome = (Income) outcomeList.get(i);
       set.add(outcome.getDate());
     }
